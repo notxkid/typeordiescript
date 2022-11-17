b1ae5e6247e1f05204dbe6c43938e86b88d5418f
xkidhehe <96038811+notxkid@users.noreply.github.com>
2022-11-16 21:22:44 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 --[[
-  - script by xkid#1299, msg me on Discord if you get any error message or if you want any suggestions.
-  
+  - script by xkid#1299, msg me on Discord if you get any error message or if you want any suggestions.  
   - Obfuscator by herrtt.
 ]]--
 
